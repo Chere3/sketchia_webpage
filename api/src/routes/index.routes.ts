a4c8ef1ea4506990092b2559c77008dddc99cf7b
Cheree <71246795+Chere3@users.noreply.github.com>
2022-10-28 06:54:07 -0500

    🔌 Agregado de endpoints base.

--- api/src/routes/index.routes.ts
+++ api/src/routes/index.routes.ts
@@ -3,8 +3,18 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
+    res.redirect("/home")
+});
+
+router.get("/home", (req, res) => {
     res.send("Hello World!");
 });
+
+router.get("/*", (req, res) => {
+    res.send("Hello World!");
+});
+
+router
 
 
 module.exports = router;
--- api/src/routes/users.routes.ts
+++ api/src/routes/users.routes.ts
@@ -2,4 +2,20 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+router.post("/add", (req, res) => {
+    res.send("Hello World!");
+});
+
+router.patch("/edit", (req, res) => {
+    res.send("Hello World!");
+});
+
+router.delete("/delete", (req, res) => {
+    res.send("Hello World!");
+});
+
 module.exports = router;
