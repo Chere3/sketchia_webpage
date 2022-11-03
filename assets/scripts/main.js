ad0e02c98cbb142db42db4b6c3b55c8fcf5f18e6
Cheree <71246795+Chere3@users.noreply.github.com>
2022-11-02 21:20:41 -0600

    Eliminados todos los archivos de la página anterior

--- assets/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// Inicia el gradiente de fondo.
-
-var gradient = new Gradient()
-gradient.initGradient('#gradient-canvas');
-
-// Cambiar de color el boton al poner el cursor encima.
-var button = document.getElementById('button');
-
