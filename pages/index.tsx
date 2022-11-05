e5802cc25e218d86f2029237b38efba66dfcca13
Cheree <71246795+Chere3@users.noreply.github.com>
2022-11-05 17:33:00 -0600

    Cambiada resolución del header al igual que agregado texto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
+import localFont from '@next/font/local'
+import { DM_Sans } from '@next/font/google'
 import { NextScript } from 'next/document'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Script from 'next/script'
 import styles from '../styles/Home.module.css'
 
+
+const extrafett = localFont({src: '../public/fonts/sohne-extrafett-webfont.woff2', variable: '--extrafett', style: 'normal', preload: true });
+const dm_sans = DM_Sans({variable: '--dm_sans', preload: true, weight: "700", style: 'normal'})
 
 export default function Home() {
   return (
@@ -12,23 +18,32 @@ export default function Home() {
         <title>Sketchia</title>
         <meta name="description" content="Empieza a aprender e instruir con sketchia." />
         <link rel="icon" href="/favicon.ico" />
-        <script src="https://pyywzoboulzbhbnihayt.supabase.co/storage/v1/object/public/recursos/gradient.js?t=2022-11-04T17%3A54%3A05.903Z"></script>
-        <script src="https://pyywzoboulzbhbnihayt.supabase.co/storage/v1/object/public/recursos/main.js?t=2022-11-04T17%3A32%3A01.643Z"></script>
       </Head>
 
       <main className={styles.header}>
+      <Script src="https://pyywzoboulzbhbnihayt.supabase.co/storage/v1/object/public/recursos/gradient.js?t=2022-11-04T17%3A54%3A05.903Z" onLoad={
+        () => {
+
+          // @ts-ignore
+          var gradient = new Gradient()
+          gradient.initGradient('.Home_gradient_canvas__cZzsk');
+          console.log('Iniciado gradiente.');
+        }
+      }></Script>
       <canvas className={styles.gradient_canvas} />
         <div className={styles.header__container}>
+          <div className={dm_sans.className}>
           <div className={styles.header__container__text}>
             <Image src="/sketchia.svg" alt="Sketchia logo" className={styles.logo} width={180} height={100} />
             <div className={styles.header__container__text__pages}>
-            <a href='/' className={styles.header__container__text__description}>Página principal</a>
-            <a href='/courses' className={styles.header__container__text__description}>Cursos</a>
-            <a href='/blog' className={styles.header__container__text__description}>Blog</a>
-            <a href='/about' className={styles.header__container__text__description}>Acerca de nosotros</a>
-            <a href='/source' className={styles.header__container__text__description}>Código fuente</a>
+            <Link href='/' className={styles.header__container__text__description}>Página principal</Link>
+            <Link href='/courses' className={styles.header__container__text__description}>Cursos</Link>
+            <Link href='/blog' className={styles.header__container__text__description}>Blog</Link>
+            <Link href='/about' className={styles.header__container__text__description}>Acerca de nosotros</Link>
+            <Link href='/source' className={styles.header__container__text__description}>Código fuente</Link>
             </div>
-            <a href='/login' className={styles.header__container__text__button}>Iniciar sesión</a>
+            <Link href='/login' className={styles.header__container__text__button}>Iniciar sesión</Link>
+          </div>
           </div> 
           <div className='header__container__image'>
           </div>
@@ -37,13 +52,35 @@ export default function Home() {
       </main>
 
       <main className={styles.main}>
+      <Script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11" onLoad={
+        () => {
+
+          // @ts-ignore
+          var typed = new Typed('.Home_italic___ByUC', {
+            // poner cosas para ensaeñar
+            strings: ['Programar', 'Diseñar', 'Cocinar', 'Enseñar', 'Estandarizar', 'Hablar otro idioma', 'Bailar', 'Cantar', 'Llorar', 'Filosofar', 'Pensar', 'Imaginar', 'Querer', 'Amar', 'Ser', 'Vivir', 'Crear esta página web', 'Relajarte de manera rápida', 'Dormir bien', 'Comer rápido', 'Beber de cabeza', 'Jugar de forma profesional', 'Escribir rápido', 'Leer con naturalidad', 'Escuchar', 'Ver', 'Hacer', 'Hablar', 'Pensar', 'Imaginar', 'Querer', 'Amar', 'Ser', 'Vivir', 'Crear esta página web', 'Relajarte', 'Dormir', 'Comer', 'Beber', 'Jugar', 'Escribir', 'Leer', 'Escuchar', 'Ver', 'Hacer', 'Hablar', 'Pensar', 'Imaginar', 'Querer', 'Amar', 'Ser', 'Vivir', 'Crear esta página web', 'Relajarte', 'Dormir', 'Comer', 'Beber', 'Jugar', 'Escribir', 'Leer', 'Escuchar', 'Ver', 'Hacer', 'Hablar', 'Pensar', 'Imaginar', 'Querer', 'Amar', 'Ser', 'Vivir', 'Crear esta página web', 'Relajarte', 'Dormir', 'Comer', 'Beber', 'Jugar', 'Escribir', 'Leer', 'Escuchar', 'Ver', 'Hacer', 'Hablar', 'Pensar', 'Imaginar', 'Querer', 'Amar', 'Ser', 'Vivir', 'Crear esta página web', 'Relajarte', 'Dormir', 'Comer', 'Beber', 'Jugar', 'Escribir', 'Leer', 'Escuchar', 'Ver', 'Hacer', 'Hablar', 'Pensar', 'Imaginar', 'Querer', 'Amar', 'Ser', 'Vivir', 'Crear esta página web', 'Relajarte', 'Dormir', 'Comer', 'Beber', 'Jugar', 'Escribir', 'Leer', 'Escuchar', 'Ver', 'Hacer', 'Hablar', 'Pensar', 'Imaginar', 'Querer', 'Amar', 'Ser', 'Vivir', 'Crear esta página web', 'Relajarte', 'Dormir', 'Comer de forma elegante', 'Quitar el estrés', 'Hacer yoga', 'Escribir rápido en el teclado', 'Leer muy rápido'],
+            typeSpeed: 50,
+            backSpeed: 50,
+            backDelay: 1000,
+            loop: true,
+            loopCount: Infinity,
+            showCursor: true,
+            cursorChar: '|',
+        });
+
+        console.log('Inicializado escritura dinamica');
+        }
+
+      } />
         <h1 className={styles.title}>
-          Aprende a <br /><span className={styles.italic}></span><br /> con Sketchia.
+          <div className={extrafett.className}>Aprende a <br /><span className={styles.italic}></span><br /> con Sketchia.</div>
         </h1>
-        <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11" />
+        <div className={dm_sans.className}>
         <p className={styles.description}>
-          Más de 3000 personas estan aprendiendo actualmente con Sketchia.
+          Todos los días miles de personas de todo México inician<br /> sesión en Sketchia para aprender con las herramientas<br /> más avanzadas hasta el momento.
         </p>
+        <Link href='/auth' className={styles.title__button__primary}>Comienza ahora</Link>
+        </div>
         <div className={styles.grid}>
          
         </div>
