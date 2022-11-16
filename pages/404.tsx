73be87a48defc6859f5990a9e696c87d96a4c0e9
--global <71246795+Chere3@users.noreply.github.com>
2022-11-16 19:50:54 +0000

    Quitados analisis de 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 import Styles from "../styles/404/404.module.css";
 import Link from "next/link";
 import { Poppins } from "@next/font/google";
+import { AnalyticsScript } from ".";
 
 const extrafett = localFont({
   src: "../public/fonts/sohne-extrafett-webfont.woff2",
@@ -29,38 +30,8 @@ export default function Custom404() {
         <meta name="description" content="Contenido no encontrado" />
       </Head>
 
+      <AnalyticsScript />
       <main className={headerStyles.header}>
-        <Script
-          id="a"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "ehcxoxhdt0");`,
-          }}
-        />
-
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-WB48WCP5H0"
-          onLoad={() => {
-            // @ts-ignore
-            window.dataLayer = window.dataLayer || [];
-            // @ts-ignore
-            function gtag() {
-              // @ts-ignore
-              dataLayer.push(arguments);
-            }
-            // @ts-ignore
-            gtag("js", new Date());
-
-            // @ts-ignore
-            gtag("config", "G-WB48WCP5H0");
-          }}
-        ></Script>
       </main>
 
       <main className={Styles.main}>
