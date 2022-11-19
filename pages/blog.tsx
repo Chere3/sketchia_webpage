1a603a5f4671d1eb34db7004bb8181511f04ec9f
--global <71246795+Chere3@users.noreply.github.com>
2022-11-19 16:45:39 +0000

    Agregado de diferentes correcciones de er

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -35,7 +35,7 @@ function BlogCard() {
                         <h1 className={blogCarrouselStyles.blogCarrousel__container__card__title__text}>Blog #1</h1>
                     </div>
                     <div className={blogCarrouselStyles.blogCarrousel__container__card__description}>
-                        <p className={blogCarrouselStyles.blogCarrousel__container__card__description__text}>Este es un blog, gracias a esta blog se pueden mostrar diferentes páginas diferentes y el como se muestran.</p>
+                        <p className={blogCarrouselStyles.blogCarrousel__container__card__description__text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
         </Link>
